5853711fd032c240c24e0579eb9be40bbfb2565f
jprio <jeanpierre.rio@gmail.com>
2023-04-01 16:33:15 +0200

    Deserialize response

---
+++ README.md
@@ -0,0 +1,6 @@
+Serves cat pictures as ASCII art over the internet.
+
+
+Références : 
+* https://fasterthanli.me/series/building-a-rust-service-with-nix/part-3
+
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,22 @@
+use serde::Deserialize;
+
 #[tokio::main]
 async fn main() {
+    #[derive(Deserialize)]
+    struct CatImage {
+        url: String,
+    }
     let res = reqwest::get("https://api.thecatapi.com/v1/images/search")
         .await
         .unwrap();
-    println!("Status: {}", res.status());
-    let body = res.text().await.unwrap();
-    println!("Body: {}", body);
+    if !res.status().is_success() {
+        panic!("Request failed with HTTP {}", res.status());
+    }
+
+    //              👇
+    let images: Vec<CatImage> = res.json().await.unwrap();
+    let image = images
+        .first()
+        .expect("the cat API should return at least one image");
+    println!("The image is at {}", image.url);
 }
